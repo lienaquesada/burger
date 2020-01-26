b3fc21d0da898d41a52756f0b200b8ef0f54da30
JaneBane <lienaqperez19@gmail.com>
2020-01-26 01:23:34 -0500

    heroku issues

--- server.js
+++ server.js
@@ -3,7 +3,7 @@ var express = require("express");
 var bodyParser = require("body-parser");
 var methodOverride = require("method-override");
 
-var port = process.env.PORT || 3000;
+let port = process.env.PORT || 3000;
 
 var app = express();
 
